b5609ba00414d85cffd03c4018333a7cbabcca0b
rezafahlevi-TIUIR <rezafahleviex@gmail.com>
2020-11-30 07:56:34 +0700

    Update and delete

--- auth.php
+++ auth.php
@@ -7,7 +7,6 @@ $dbname = "myapp";
 date_default_timezone_set('Asia/Jakarta');
 $time = date('Y-m-d H:i:s');
 $conn = new mysqli($servername, $username, $password, $dbname);
-echo "aman";
 /* check connection */
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
